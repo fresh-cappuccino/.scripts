e51993a92a76d908474fab71f58793b3bc3e2868
Leo Andrade <od.leonardo01@gmail.com>
2021-01-05 20:55:24 -0300

    updating configs

--- dwmstatusbar/dwm_statusbar.sh
+++ dwmstatusbar/dwm_statusbar.sh
@@ -7,8 +7,8 @@ dte()
 
 mem()
 {
-	mem_used_=$(echo "($(cat /proc/meminfo|sed -n 1p|tr -d [A-Za-z:' ']) - $(cat /proc/meminfo|sed -n 2p|tr -d [A-Za-z:' '])) / 1000"|bc)
-	mem_tot_=$(echo $(cat /proc/meminfo | sed -n 1p | tr -d [A-Za-z:' ']) / 1000 | bc)" MiB"
+	mem_used_=$(echo "(`cat /proc/meminfo|sed -n 1p|tr -d [A-Za-z:' ']` - `cat /proc/meminfo|sed -n 2p|tr -d [A-Za-z:' ']`) / 1000"|bc)
+	mem_tot_=$(echo "`cat /proc/meminfo|sed -n 1p|tr -d [A-Za-z:' ']` / 1000"|bc)" MiB"
 	mem_="[📏] "$mem_used_"/"$mem_tot_
 
 }
