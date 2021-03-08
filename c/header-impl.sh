e8ec61945a68003a832c1a7d38d1fddd1adbb6b6
Leo Andrade <od.leonardo01@gmail.com>
2021-03-07 21:14:18 -0300

    updating c scripts

--- c/header-impl.sh
+++ c/header-impl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[[ -z $1 ]] && echo "You need to specify a name for the library" && exit
+[[ -z $1 ]] && echo "You need to specify a name for the library implementation" && exit
 
 header_name=$1
 [[ `echo $header_name|awk -F '.' '{print $NF}'` = "h" ]] && header_name=${header_name:0:$[${#header_name} - 2]}
