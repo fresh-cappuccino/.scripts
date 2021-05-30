b67d8f680aae9d634dcf36baeaa7dda64d5a5056
Leo Andrade <od.leonardo01@gmail.com>
2021-05-30 20:35:22 -0300

    updating pacman scripts

--- dwm/pacman/pacremove.sh
+++ dwm/pacman/pacremove.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-packages_=`pacman -Qt|fzf -m|awk '{ for (i = 1; i <= NF; i++) if (++j % 2 == 1) print $i; }'`
+packages_=`pacman -Qt|fzf -m --preview 'cat <(pacman -Qi {1})' |awk '{ for (i = 1; i <= NF; i++) if (++j % 2 == 1) print $i; }'`
 
-sudo pacman -Rsnc $packages_
+[ X"" = X"$packages_" ] && exit 0 || sudo pacman -Rsnc $packages_
